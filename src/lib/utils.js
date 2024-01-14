5b542db1e80d179bee66a4f257dce04e029f506d
Nurul Insani Dewi <nurulinsanid@gmail.com>
2024-01-14 22:51:04 +0700

    chore: add function library

---
+++ src/lib/utils.js
@@ -0,0 +1,11 @@
+//** FUNCTION LIBRARY */
+
+// capitalize first letter
+export function capitalizeFirstLetter(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+  
+  // current year
+  export function currentYear() {
+    return new Date().getFullYear();
+  }
